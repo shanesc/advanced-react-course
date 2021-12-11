57ee188f7a304f1f22e3e8b64deb5aca0507a3f8
Shane Mitchell <shanesc@gmail.com>
2021-12-11 13:07:16 -0800

    Setup

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,4 +1,11 @@
+import { Router } from 'next/dist/client/router'
+import Nprogress from 'nprogress'
 import Page from '../components/Page'
+import '../components/styles/nprogress.css'
+
+Router.events.on('routeChangeStart', () => Nprogress.start())
+Router.events.on('routeChangeComplete', () => Nprogress.done())
+Router.events.on('routeChangeError', () => Nprogress.done())
 
 export default function MyApp({ Component, pageProps }) {
   return (
