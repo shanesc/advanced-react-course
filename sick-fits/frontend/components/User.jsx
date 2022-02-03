3b4f0f6df138ce6d26551448052558c76ce1c9bc
Shane Mitchell <shanesc@gmail.com>
2022-02-03 15:53:39 -0800

    Add useUser hook

---
+++ sick-fits/frontend/components/User.jsx
@@ -0,0 +1,19 @@
+import { useQuery } from '@apollo/client'
+import gql from 'graphql-tag'
+
+const QUERY_AUTHENTICATED_USER = gql`
+  query QUERY_AUTHENTICATED_USER {
+    authenticatedItem {
+      ... on User {
+        id
+        name
+        email
+      }
+    }
+  }
+`
+
+export function useUser() {
+  const { data } = useQuery(QUERY_AUTHENTICATED_USER)
+  return data?.authenticatedItem
+}
