777628fc95e09d0eb29a7629381f45aa87f9ee5f
Shane Mitchell <shanesc@gmail.com>
2022-02-08 17:11:32 -0800

    Add SignUp component to the signin page

---
+++ sick-fits/frontend/components/SignUp.jsx
@@ -0,0 +1,89 @@
+import { useMutation } from '@apollo/client'
+import gql from 'graphql-tag'
+import { useRouter } from 'next/router'
+import useForm from '../lib/useForm'
+import DisplayError from './ErrorMessage'
+import Form from './styles/Form'
+import { QUERY_AUTHENTICATED_USER } from './User'
+
+const MUTATION_SIGN_IN_USER = gql`
+  mutation MUTATION_SIGN_IN_USER($email: String!, $password: String!) {
+    authenticateUserWithPassword(email: $email, password: $password) {
+      ... on UserAuthenticationWithPasswordSuccess {
+        item {
+          id
+          name
+          email
+        }
+      }
+      ... on UserAuthenticationWithPasswordFailure {
+        code
+        message
+      }
+    }
+  }
+`
+
+export default function SignUp() {
+  const router = useRouter()
+
+  const { inputs, handleChange } = useForm({
+    email: '',
+    password: '',
+  })
+
+  const [signIn, { data }] = useMutation(MUTATION_SIGN_IN_USER, {
+    variables: inputs,
+    refetchQueries: [{ query: QUERY_AUTHENTICATED_USER }],
+  })
+
+  async function handleSubmit() {
+    const {
+      data: {
+        authenticateUserWithPassword: { code },
+      },
+    } = await signIn()
+    return code
+  }
+
+  return (
+    <Form
+      method="POST"
+      onSubmit={async (e) => {
+        e.preventDefault()
+        const code = await handleSubmit()
+        if (!code) {
+          router.push('/products')
+        }
+      }}
+    >
+      <fieldset>
+        <DisplayError error={data?.authenticateUserWithPassword} />
+        <h2>Sign in to your account</h2>
+        <label>
+          Email
+          <input
+            type="email"
+            name="email"
+            placeholder="Email Address"
+            autoComplete="email"
+            value={inputs.email}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Password
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            autoComplete="password"
+            value={inputs.password}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Sign In</button>
+      </fieldset>
+    </Form>
+  )
+}
--- sick-fits/frontend/pages/signin.js
+++ sick-fits/frontend/pages/signin.js
@@ -1,5 +1,18 @@
+import styled from 'styled-components'
 import SignIn from '../components/SignIn'
+import SignUp from '../components/SignUp'
+
+const StyledGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+  gap: 2rem;
+`
 
 export default function SignInPage() {
-  return <SignIn />
+  return (
+    <StyledGrid>
+      <SignIn />
+      <SignUp />
+    </StyledGrid>
+  )
 }
