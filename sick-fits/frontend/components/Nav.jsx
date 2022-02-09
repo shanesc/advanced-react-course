bf5e5ed5a8af268a00870c594c2b0b7f671d3ad4
Shane Mitchell <shanesc@gmail.com>
2022-02-08 16:50:06 -0800

    Add useSignOutUser hook

--- sick-fits/frontend/components/Nav.jsx
+++ sick-fits/frontend/components/Nav.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import Link from 'next/link'
 import NavStyles from './styles/NavStyles'
 import { useUser } from './User'
+import { useSignOutUser } from '../lib/useSignOutUser'
 
 export default function Nav() {
   const user = useUser()
+  const { signOutUser } = useSignOutUser()
   return (
     <NavStyles>
       <Link href="/products">Products</Link>
@@ -13,6 +15,9 @@ export default function Nav() {
           <Link href="/sell">Sell</Link>
           <Link href="/orders">Orders</Link>
           <Link href="/account">Account</Link>
+          <button type="button" onClick={signOutUser}>
+            Sign Out
+          </button>
         </>
       ) : (
         <Link href="/signin">Sign In</Link>
