6eca2cbf22ac84b62736822f4a91f79ca86d6494
Shane Mitchell <47223963+shanesc@users.noreply.github.com>
2022-02-15 17:42:52 -0800

    Add email handler for password reset link (#5)

--- sick-fits/frontend/pages/reset.js
+++ sick-fits/frontend/pages/reset.js
@@ -2,11 +2,11 @@ import RequestReset from '../components/RequestReset'
 import ResetPassword from '../components/ResetPassword'
 
 export default function ResetPage({ query }) {
-  const { token } = query
+  const { token, identity } = query
 
   if (!token) {
     return <RequestReset />
   }
 
-  return <ResetPassword token={token} />
+  return <ResetPassword token={token} email={identity} />
 }
